a6f84fe68c324de684993207e8ec22e013a667cd
jefferybai <13518552+jefferybai@users.noreply.github.com>
2022-12-06 17:01:14 +0800

    Update promise.js

--- promise.js
+++ promise.js
@@ -1,20 +1,44 @@
-new Promise((res, reject) => res(1)).then(() => {});
+new Promise((reslove, reject) => res(1)).then(() => {});
 
-function myPromise(fn) {
-  const PENDDING = 'pendding';
-  const RESOLVE = 'resolve';
-  const REJECT = 'reject';
-  let resolveCallbacks = [];
-  let rejectCallbacks = [];
-  this.then = (onFullFn, onRejectFn) => {
-    resolveCallbacks.push(onFullFn);
-    rejectCallbacks.push(onRejectFn);
-  };
+const PENDDING = 'pendding';
+const RESOLVE = 'resolve';
+const REJECT = 'reject';
 
-  function reslove() {
+function MyPromise(fn) {
+
+  this.resolveCallbacks = [];
+  this.rejectCallbacks = [];
+  this.value;
+  this.rejectValue;
+  this.status = PENDDING;
+  let that = this;
+
+  function resolve(res) {
+    this.status = RESOLVE;
+    this.value = res;
+    that.resolveCallbacks.forEach((resFn) => resFn(this.value));
   }
 
-  function reject() {}
+  function reject(error) {
+    this.status = REJECT;
+    this.rejectValue = error;
+    that.rejectCallbacks.forEach((resFn) => resFn(this.rejectValue));
+  }
 
-  fn(reslove, reject)
+  fn(resolve, reject)
 }
+
+MyPromise.prototype.then = function (onFullFn, onRejectFn) {
+  if(this.status === RESOLVE) {
+    onFullFn(this.value);
+  } else if(this.status === REJECT) {
+    onRejectFn(this.value);
+  }
+  if(this.status === PENDDING) {
+    this.resolveCallbacks.push(onFullFn);
+    this.rejectCallbacks.push(onRejectFn);
+  }
+  return this;
+};
+
+new MyPromise((res, rej) => setTimeout(() => res(3333), 1000)).then((res) => console.log(res));
