4b35aa1729bece3a44a030f21e3048c05e93cabb
jefferybai <13518552+jefferybai@users.noreply.github.com>
2022-12-06 17:52:12 +0800

    Update apply.js

--- apply.js
+++ apply.js
@@ -1,15 +1,22 @@
-function fn(params) {}
+// function fn(params) {}
 
-fn.apply(this, [1, 2, 3, 4]);
+// fn.apply(this, [1, 2, 3, 4]);
 
 Function.prototype.myApply = function () {
   const fn = this;
+  const args = [...arguments];
   if (typeof fn !== 'function') {
     throw new Error('非方法');
   }
-  const context = arguments.slice(0, 1);
-  const symbol = new symbol();
+  const context = args.slice(0, 1)[0];
+  const symbol = Symbol();
   context[symbol] = fn;
-  context[symbol](arguments.slice(1));
+  context[symbol](args.slice(1));
   delete context[symbol];
-};
+}
+
+const fn = function() {
+ console.log(this.a);
+}
+
+fn.myApply({a: 12}, []);
